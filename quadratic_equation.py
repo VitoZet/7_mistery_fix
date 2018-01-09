3d30a9b46606fbf0049808b4cc85a61f514e9c72
VitoZet <slava@gigel.ru>
2018-01-09 23:02:45 +0300

    отрицательный дискриминант

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,11 +3,15 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
-    root0 = -b / (2 * a)
 
     if discriminant == 0:
-        return root0
+        root1 = (-b) / (2 * a)
+        return root1, None
+
+    elif discriminant < 0:
+        return None, None
+
     else:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
         return root1, root2
